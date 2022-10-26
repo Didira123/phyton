b3e7f02475d0c75da2d5a5813ed14183ae0b9195
Diego <ogeiddiego064@gmail.com>
2022-10-26 19:45:28 -0300

    teste

--- UC9-Atividade_1_Phyton/Exercicio_2.py
+++ UC9-Atividade_1_Phyton/Exercicio_2.py
@@ -1,7 +1,7 @@
-print("Diga caracteres (não números): ")
+print("Diga caracteres (não números): ", end="")
 a = str(input())
 
-print("Diga um valor inteiro: ")
+print("Diga um valor inteiro: ", end="")
 b = int(input())
 
 print("Valor em caracteres: "+a)
