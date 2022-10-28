0539b73d1041af9a1d37da4a3e5520be199fb6f3
Diego <ogeiddiego064@gmail.com>
2022-10-28 19:52:46 -0300

    UC9-Atividade 3 Phyton

--- UC9/UC9-Atividade_3_Phyton/Exercicio3.py
+++ UC9/UC9-Atividade_3_Phyton/Exercicio3.py
@@ -1,71 +1,84 @@
 print("Diga uma operação entre 2 números digitando o 1º número, seguido da operação")
 print("(digito único), seguida do outro número (sem espaços).", end="")
 fim = "s"
+ANS = 0
 while fim.lower() == "s":
     print("Calculo: ", end="")
-    calculo = input()
-
+    calculo = input().replace(" ", "")
     # Calcula a conta na String automaticamente:
     #
     # calculo = eval(input())
     # print (calculo)
 
-    ANS = 0
-
     if len(calculo.split("^")) > 1:
         a = calculo.split("^")
-
         if a[0].casefold().upper() == "ANS":
-            print(ANS**float(a[1]))
+            ANS **= float(a[1])
         elif a[1].casefold().upper() == "ANS":
-            print(float(a[0])**ANS)
+            ANS = float(a[0])**ANS
+        elif a[0].casefold().upper() == a[1].casefold().upper() == "ANS":
+            ANS **= ANS
         else:
-            print(float(a[0]) ** float(a[1]))
-
+            ANS = float(a[0]) ** float(a[1])
+        print(ANS)
     elif len(calculo.split("*")) > 1:
         a = calculo.split("*")
         if a[0].casefold().upper() == "ANS":
-            print(ANS*float(a[1]))
+            ANS *= float(a[1])
         elif a[1].casefold().upper() == "ANS":
-            print(float(a[0])*ANS)
+            ANS *= float(a[0])
+        elif a[0].casefold().upper() == a[1].casefold().upper() == "ANS":
+            ANS *= ANS
         else:
-            print(float(a[0]) * float(a[1]))
-
+            ANS = float(a[0]) * float(a[1])
+        print(ANS)
     elif len(calculo.split("/")) > 1:
         a = calculo.split("/")
         if a[0].casefold().upper() == "ANS":
-            print(ANS/float(a[1]))
+            ANS /= float(a[1])
         elif a[1].casefold().upper() == "ANS":
-            print(float(a[0])/ANS)
+            ANS = float(a[0])/ANS
+        elif a[0].casefold().upper() == a[1].casefold().upper() == "ANS":
+            ANS = 1
         else:
-            print(float(a[0]) / float(a[1]))
-
+            ANS = float(a[0]) / float(a[1])
+        print(ANS)
     elif len(calculo.split("+")) > 1:
         a = calculo.split("+")
         if a[0].casefold().upper() == "ANS":
-            print(ANS + float(a[1]))
+            ANS += float(a[1])
         elif a[1].casefold().upper() == "ANS":
-            print(float(a[0])+ANS)
+            ANS += float(a[0])
+        elif a[0].casefold().upper() == a[1].casefold().upper() == "ANS":
+            ANS += ANS
         else:
-            print(float(a[0])+float(a[1]))
+            ANS = float(a[0])+float(a[1])
+        print(ANS)
     elif len(calculo.split("-")) > 1:
         a = calculo.split("-")
         if a[0].casefold().upper() == "ANS":
-            print(ANS-float(a[1]))
+            ANS -= float(a[1])
         elif a[1].casefold().upper() == "ANS":
-            print(float(a[0])-ANS)
+            ANS = float(a[0])-ANS
+        elif a[0].casefold().upper() == a[1].casefold().upper() == "ANS":
+            ANS = 0
         else:
-            print(float(a[0]) - float(a[1]))
+            ANS = float(a[0]) - float(a[1])
+        print(ANS)
     elif len(calculo.split("%")) > 1:
         a = calculo.split("%")
         if a[0].casefold().upper() == "ANS":
-            print(ANS%float(a[1]))
+            ANS %= float(a[1])
         elif a[1].casefold().upper() == "ANS":
-            print(float(a[0])%ANS)
+            ANS = float(a[0])%ANS
+        elif a[0].casefold().upper() == a[1].casefold().upper() == "ANS":
+            ANS = 0
         else:
-            print(float(a[0]) % float(a[1]))
-    else :
-        print("Operação não implementada!")
+            ANS = float(a[0]) % float(a[1])
+        print(ANS)
+    else:
+        print("Operação não implementada OU valor inválido!")
 
     print("Continuar operação da calculadora? (s/n)")
     fim = input()
+    if fim.upper() == "S": print("ANS = "+str(ANS))
