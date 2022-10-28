65dac661e33a7575bff3b6d50a184a881ab28a7f
Diego <ogeiddiego064@gmail.com>
2022-10-27 21:55:47 -0300

    UC9-Atividade_3_Phyton

--- UC9/UC9-Atividade_3_Phyton/Exercicio3.py
+++ UC9/UC9-Atividade_3_Phyton/Exercicio3.py
@@ -1,30 +1,71 @@
-print("Diga uma operação entre 2 números digitando o 1º número,")
-print("seguido da operação (digito único), seguida do outro número (sem espaços): ", end="")
+print("Diga uma operação entre 2 números digitando o 1º número, seguido da operação")
+print("(digito único), seguida do outro número (sem espaços).", end="")
+fim = "s"
+while fim.lower() == "s":
+    print("Calculo: ", end="")
+    calculo = input()
 
-calculo = input()
+    # Calcula a conta na String automaticamente:
+    #
+    # calculo = eval(input())
+    # print (calculo)
 
-# Calcula a conta na String automaticamente:
-#
-# calculo = eval(input())
-# print (calculo)
+    ANS = 0
 
-if len(calculo.split("^")) > 1:
-    a = calculo.split("^")
-    print(float(a[0]) ** float(a[1]))
-elif len(calculo.split("*")) > 1:
-    a = calculo.split("*")
-    print(float(a[0]) * float(a[1]))
-elif len(calculo.split("/")) > 1:
-    a = calculo.split("/")
-    print(float(a[0]) / float(a[1]))
-elif len(calculo.split("+")) > 1:
-    a = calculo.split("+")
-    print(float(a[0])+float(a[1]))
-elif len(calculo.split("-")) > 1:
-    a = calculo.split("-")
-    print(float(a[0]) - float(a[1]))
-elif len(calculo.split("%")) > 1:
-    a = calculo.split("%")
-    print(float(a[0]) % float(a[1]))
-else :
-    print("Operação não implementada!")
+    if len(calculo.split("^")) > 1:
+        a = calculo.split("^")
+
+        if a[0].casefold().upper() == "ANS":
+            print(ANS**float(a[1]))
+        elif a[1].casefold().upper() == "ANS":
+            print(float(a[0])**ANS)
+        else:
+            print(float(a[0]) ** float(a[1]))
+
+    elif len(calculo.split("*")) > 1:
+        a = calculo.split("*")
+        if a[0].casefold().upper() == "ANS":
+            print(ANS*float(a[1]))
+        elif a[1].casefold().upper() == "ANS":
+            print(float(a[0])*ANS)
+        else:
+            print(float(a[0]) * float(a[1]))
+
+    elif len(calculo.split("/")) > 1:
+        a = calculo.split("/")
+        if a[0].casefold().upper() == "ANS":
+            print(ANS/float(a[1]))
+        elif a[1].casefold().upper() == "ANS":
+            print(float(a[0])/ANS)
+        else:
+            print(float(a[0]) / float(a[1]))
+
+    elif len(calculo.split("+")) > 1:
+        a = calculo.split("+")
+        if a[0].casefold().upper() == "ANS":
+            print(ANS + float(a[1]))
+        elif a[1].casefold().upper() == "ANS":
+            print(float(a[0])+ANS)
+        else:
+            print(float(a[0])+float(a[1]))
+    elif len(calculo.split("-")) > 1:
+        a = calculo.split("-")
+        if a[0].casefold().upper() == "ANS":
+            print(ANS-float(a[1]))
+        elif a[1].casefold().upper() == "ANS":
+            print(float(a[0])-ANS)
+        else:
+            print(float(a[0]) - float(a[1]))
+    elif len(calculo.split("%")) > 1:
+        a = calculo.split("%")
+        if a[0].casefold().upper() == "ANS":
+            print(ANS%float(a[1]))
+        elif a[1].casefold().upper() == "ANS":
+            print(float(a[0])%ANS)
+        else:
+            print(float(a[0]) % float(a[1]))
+    else :
+        print("Operação não implementada!")
+
+    print("Continuar operação da calculadora? (s/n)")
+    fim = input()
